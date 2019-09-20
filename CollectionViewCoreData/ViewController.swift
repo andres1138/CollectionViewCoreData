29090783fc0276879d4066ced9d4d098ab79b62c
Andre Sarokhanian <andres1983@icloud.com>
2019-09-20 14:10:36 -0500

    Initial Commit

---
+++ CollectionViewCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewCoreData
+//
+//  Created by Andre Sarokhanian on 9/20/19.
+//  Copyright © 2019 Andre Sarokhanian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
